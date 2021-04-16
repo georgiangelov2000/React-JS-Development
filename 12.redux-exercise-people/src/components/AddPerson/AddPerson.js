0089b2e85350efc91f74807688466981eea90c73
George <angelov2000@abv.bg>
2021-04-16 19:37:19 +0300

    Added form

--- 12.redux-exercise-people/src/components/AddPerson/AddPerson.js
+++ 12.redux-exercise-people/src/components/AddPerson/AddPerson.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import './AddPerson.css';
+import React, { Component } from "react";
+import "./AddPerson.css";
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class addPerson extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: "",
+    };
+    this.onNameChangeHandler= this.onNameChangeHandler.bind(this);
+    this.onAgeChangeHandler=this.onAgeChangeHandler.bind(this);
+  }
+
+    onNameChangeHandler(event){
+        this.setState({
+            name:event.target.value
+        })
+    }
+
+    onAgeChangeHandler(event){
+        this.setState({
+            age:event.target.value
+        })
+    }
+
+  render() {
+    return (
+      <div className="AddPerson">
+        <input 
+        type="text" 
+        name="name" 
+        placeholder="Enter name" 
+        onChange={this.onNameChangeHandler}
+        value={this.state.name}
+        />
+        <input 
+        type="number" 
+        name="age" 
+        placeholder="Enter Age"
+        onChange={this.onAgeChangeHandler}
+        value={this.state.age}
+        />
+        <button onClick={()=>this.props.personAdded(this.state.name,this.state.age)}>Add Person</button>
+      </div>
+    );
+  }
+}
 
 export default addPerson;
