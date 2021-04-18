3d17c86022b57297ded8bc50ab63fe3f88d79666
George <angelov2000@abv.bg>
2021-04-18 12:02:30 +0300

    completed

--- 13.expenses/src/App.js
+++ 13.expenses/src/App.js
@@ -1,54 +1,45 @@
-import React, { Component } from "react";
-import Expenses from "./components/Expenses";
-import NewExpense from "./components/NewExpense";
+import React, { useState } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expenses: [
-        {
-          id: "e1",
-          title: "Toilet Paper",
-          amount: 94.12,
-          date: new Date(2020, 7, 14),
-        },
-        {
-          id: "e2",
-          title: "New TV",
-          amount: 799.49,
-          date: new Date(2021, 2, 12),
-        },
-        {
-          id: "e3",
-          title: "Car Insurance",
-          amount: 294.67,
-          date: new Date(2021, 2, 28),
-        },
-        {
-          id: "e4",
-          title: "New Desk (Wooden)",
-          amount: 450,
-          date: new Date(2021, 5, 12),
-        },
-      ],
-    };
-  }
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
-  addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
-
-  render() {
-    return (
-      <div>
-        <h2>Let's get started!</h2>
-        <NewExpense onAddExpense={this.addExpenseHandler} />
-        <Expenses items={this.state.expenses} />
-      </div>
-    );
-  }
-}
+  
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
 
 export default App;
