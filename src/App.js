78feaf48c8090fd24738405bdaca32a17a0d7868
George <angelov2000@abv.bg>
2021-04-22 17:17:54 +0300

    added react api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+import "./App.css"
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>Please fetch data for tours</h2>
+          <button className='btn' onClick={() => fetchData()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return(
+    <main>
+      <Tours tours={tours} removeTour={removeTour}  />
+    </main>
+  )
 }
 
 export default App;
