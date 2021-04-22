72989004b4dd811524d62d132cc0c6cd2d235d99
SoftAcademy/Plovdiv University/Georgi Angelov <60781748+georgiangelov2000@users.noreply.github.com>
2021-04-22 18:30:27 +0300

    Revert "added react api"
    This reverts commit 78feaf48c8090fd24738405bdaca32a17a0d7868.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
-import React, { useState, useEffect } from "react";
-import Loading from "./components/Loading";
-import Tours from "./components/Tours";
-import "./App.css"
-const url = "https://course-api.com/react-tours-project";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
-
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className='title'>
-          <h2>Please fetch data for tours</h2>
-          <button className='btn' onClick={() => fetchData()}>
-            Refresh
-          </button>
-        </div>
-      </main>
-    );
-  }
-
-  return(
-    <main>
-      <Tours tours={tours} removeTour={removeTour}  />
-    </main>
-  )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
